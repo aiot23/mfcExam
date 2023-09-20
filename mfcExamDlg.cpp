12d6360fa053a05254b5687992b64e20469f3012
hanseol <145195969+aiot23@users.noreply.github.com>
2023-09-20 14:40:55 +0900

    insert DlgImage
    이미지 다이얼로그를 삽입함

--- mfcExamDlg.cpp
+++ mfcExamDlg.cpp
@@ -8,10 +8,12 @@
 #include "mfcExamDlg.h"
 #include "afxdialogex.h"
 
+
 #ifdef _DEBUG
 #define new DEBUG_NEW
 #endif
 
+#pragma comment(linker, "/entry:WinMainCRTStartup /subsystem:console")
 
 // 응용 프로그램 정보에 사용되는 CAboutDlg 대화 상자입니다.
 
@@ -66,6 +68,7 @@ BEGIN_MESSAGE_MAP(CmfcExamDlg, CDialogEx)
 	ON_WM_PAINT()
 	ON_WM_QUERYDRAGICON()
 	ON_BN_CLICKED(IDC_BTN_DLG, &CmfcExamDlg::OnBnClickedBtnDlg)
+	ON_WM_DESTROY()
 END_MESSAGE_MAP()
 
 
@@ -101,6 +104,10 @@ BOOL CmfcExamDlg::OnInitDialog()
 	SetIcon(m_hIcon, FALSE);		// 작은 아이콘을 설정합니다.
 
 	// TODO: 여기에 추가 초기화 작업을 추가합니다.
+	m_pDlgImage = new CDlgImage;
+	m_pDlgImage->Create(IDD_DLGIMAGE, this);
+	m_pDlgImage->ShowWindow(SW_SHOW);
+
 
 	return TRUE;  // 포커스를 컨트롤에 설정하지 않으면 TRUE를 반환합니다.
 }
@@ -158,6 +165,20 @@ HCURSOR CmfcExamDlg::OnQueryDragIcon()
 
 void CmfcExamDlg::OnBnClickedBtnDlg()
 {
-	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
-	// gitTes
+	m_pDlgImage->ShowWindow(SW_SHOW);
+
+}
+
+
+void CmfcExamDlg::OnDestroy()
+{
+	CDialogEx::OnDestroy();
+
+	delete m_pDlgImage;
+}
+#include <iostream>
+void CmfcExamDlg::callFunc(int n)
+{
+	//int nData = n;
+	std::cout << n << std::endl;
 }
